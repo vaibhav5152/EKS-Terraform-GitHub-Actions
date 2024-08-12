72963e5659b276dfe56e2a044c3e9ae56e6098a3
vaibhav5152 <129995960+vaibhav5152@users.noreply.github.com>
2024-08-12 20:00:42 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,17 +1,8 @@
 terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-s33-bucket-0102"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
 }
 
